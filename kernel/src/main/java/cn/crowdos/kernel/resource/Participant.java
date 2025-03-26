3f37b21ac67baaea1214be7e2a8a6b03c07a2b39
397838177@qq.com <393838177@qq.com>
2025-03-26 09:38:41 +0800

    change head

--- kernel/src/main/java/cn/crowdos/kernel/resource/Participant.java
+++ kernel/src/main/java/cn/crowdos/kernel/resource/Participant.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2025 CrowdOS_Group, Inc. <https://github.com/crowdosNWPU/CrowdOS>
+ * Copyright 2019-2025 CrowdOS Group, Northwestern Polytechnical University, Inc. <https://github.com/crowdosNWPU/CrowdOS>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
